15c4dde92b5cc40adcae43eba7e1a69aa059936b
jhanzaib593 <100871510+jhanzaib593@users.noreply.github.com>
2023-02-14 11:26:16 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -72,38 +72,6 @@ for (var i = 0; i < btns.length; i++) {
 //js for blog categories
 
 
-//js for Poem owlCarousel
-$(function() {
-
-	if ( $('.owl-2').length > 0 ) {
-        $('.owl-2').owlCarousel({
-            center: false,
-            items: 1,
-            loop: true,
-            stagePadding: 0,
-            margin: 20,
-            smartSpeed: 1000,
-            autoplay: true,
-            nav: true,
-            dots: true,
-            pauseOnHover: false,
-            responsive:{
-                600:{
-                    margin: 20,
-                    nav: true,
-                  items: 2
-                },
-                1000:{
-                    margin: 20,
-                    stagePadding: 0,
-                    nav: true,
-                  items: 3
-                }
-            }
-        });            
-    }
-
-})
 
 
 
