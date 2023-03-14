3cf84a6fb4a7bcb573fa0fe9929c7b6fb76df014
Jhanzaib <100871510+jhanzaib593@users.noreply.github.com>
2023-03-14 12:50:13 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -72,6 +72,23 @@ for (var i = 0; i < btns.length; i++) {
 //js for blog categories
 
 
+// Read More Read Less Button in Gallery
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
 
 
 
